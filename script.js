d53895401386c1cac4cd37defa1059c8688889bb
juan francisco gonzalez cerrato <jfgonzalezc02@gmail.com>
2023-10-24 01:19:21 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const preloader = document.getElementById("preloader");
+    const verPortfolioButton = document.getElementById("verPortfolio");
+    const content = document.getElementById("content");
+
+    // Mostrar el contenido cuando se hace clic en el botón
+    verPortfolioButton.addEventListener("click", function () {
+        preloader.classList.add("hidden");
+        content.style.display = "block";
+    });
+});
