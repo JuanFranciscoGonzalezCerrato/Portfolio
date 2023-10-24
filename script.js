08375564b764519971dc166e6cf0ad2f0938a4e2
juan francisco gonzalez cerrato <jfgonzalezc02@gmail.com>
2023-10-24 10:53:59 +0200

    cambio del lenguaje y posicion del sript

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const preloader = document.getElementById("preloader");
-    const verPortfolioButton = document.getElementById("verPortfolio");
-    const content = document.getElementById("content");
 
-    // Mostrar el contenido cuando se hace clic en el botón
-    verPortfolioButton.addEventListener("click", function () {
-        preloader.classList.add("hidden");
-        content.style.display = "block";
-    });
+const preloader = document.getElementById("preloader");
+const verPortfolioButton = document.getElementById("verPortfolio");
+const content = document.getElementById("content");
+
+// Mostrar el contenido cuando se hace clic en el botón
+verPortfolioButton.addEventListener("click", function () {
+	preloader.classList.add("hidden");
+	content.style.display = "block";
 });
+
