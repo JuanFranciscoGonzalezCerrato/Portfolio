a226103b9e11af246e4796fe3f60a897b85854e1
juan francisco gonzalez cerrato <jfgonzalezc02@gmail.com>
2023-12-29 13:56:43 +0100

    he quitado el boton para entrar

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-const preloader = document.getElementById("preloader");
-const verPortfolioButton = document.getElementById("verPortfolio");
-const content = document.getElementById("content");
+// const preloader = document.getElementById("preloader");
+// const verPortfolioButton = document.getElementById("verPortfolio");
+// const content = document.getElementById("content");
 
-// Mostrar el contenido cuando se hace clic en el botón
-verPortfolioButton.addEventListener("click", function () {
-	preloader.classList.add("hidden");
-	content.style.display = "block";
-});
+// // Mostrar el contenido cuando se hace clic en el botón
+// verPortfolioButton.addEventListener("click", function () {
+// 	preloader.classList.add("hidden");
+// 	content.style.display = "block";
+// });
 
